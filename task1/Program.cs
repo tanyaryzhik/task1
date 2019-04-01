4c269211012adae924801b1038c092d1863f5ed6
Tanya <tanyaryzhik33@gmail.com>
2019-04-02 00:00:12 +0300

    added Hometask1_5

--- task1/Program.cs
+++ task1/Program.cs
@@ -39,7 +39,8 @@ namespace task1
             //Hometask1_1();
             //Hometask1_2();
             //Hometask1_3();
-            Hometask1_4();
+            //Hometask1_4();
+            Hometask1_5();
             Console.ReadLine();
         }
 
@@ -78,6 +79,19 @@ namespace task1
             string str = Console.ReadLine();
             int side = Convert.ToInt32(str);
             Console.WriteLine($"Perimeter is {4 * side}");
+        }
+
+        public static void Hometask1_5()
+        {
+            Console.WriteLine("Input R1");
+            int rad1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input R2");
+            int rad2 = Convert.ToInt32(Console.ReadLine());
+            double square1 = 3.14 * rad1 * rad1;
+            double square2 = 3.14 * rad2 * rad2;
+            double square3 = square1 - square2;
+            Console.WriteLine($"Square 1 is {square1} \nSquare 2 is {square2}");
+            Console.WriteLine($"Square of ring is {square3}");
         }
     }
 }
