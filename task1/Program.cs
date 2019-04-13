6ca30f5532011473991265a9408bc726310b563b
Tanya <tanyaryzhik33@gmail.com>
2019-04-13 12:02:37 +0300

    Added analyze of array, task 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,32 +10,33 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input radius");
-            string radiusString = Console.ReadLine();
-            int.TryParse(radiusString, out int radius);
-            Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
-            Console.ReadLine();
+            //Console.WriteLine("Input radius");
+            //string radiusString = Console.ReadLine();
+            //int.TryParse(radiusString, out int radius);
+            //Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
+            //Console.ReadLine();
 
-            //task1
-            int uberflu;
-            int _Identifier;
-            int \u006fIdentifier;
-            int myVar;
-            int myVariab1le;
+            ////task1
+            //int uberflu;
+            //int _Identifier;
+            //int \u006fIdentifier;
+            //int myVar;
+            //int myVariab1le;
 
-            //task2
-            double pi = 3.141592653;
-            double e = 2.7182818284590452;
-            Console.WriteLine($"Pi: {pi}");
-            Console.WriteLine($"e: {e}");
+            ////task2
+            //double pi = 3.141592653;
+            //double e = 2.7182818284590452;
+            //Console.WriteLine($"Pi: {pi}");
+            //Console.WriteLine($"e: {e}");
 
-            //task3
-            string str1 = "\nмоя строка";
-            string str2 = "\tмоя строка";
-            string str3 = "\aмоя строка";
-            Console.WriteLine(str1);
-            Console.WriteLine(str2);
-            Console.WriteLine(str3);
+            ////task3
+            //string str1 = "\nмоя строка";
+            //string str2 = "\tмоя строка";
+            //string str3 = "\aмоя строка";
+            //Console.WriteLine(str1);
+            //Console.WriteLine(str2);
+            //Console.WriteLine(str3);
+            arrayAnalyze();
             Console.ReadLine();
         }
 
@@ -44,6 +45,58 @@ namespace task1
             const decimal Pi = 3.14m;
             decimal square = Pi * r * r;
             return square;
+        }
+
+        public static void arrayAnalyze()
+        {
+            Console.WriteLine("Enter number of elements");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+
+            var random = new Random();
+            int[] array = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = random.Next(100);
+                Console.WriteLine(array[i]);
+            }
+
+            //
+            // Min and Max
+            //
+            int min = 100;
+            int max = 0;
+            int sum = 0;
+            double mean = 0.0;
+            string oddNumbers = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                // Max & min
+                for (int k = i + 1; k < array.Length; k++)
+                {
+                    if (array[i] > array[k] && array[i] > max)
+                    {
+                        max = array[i];
+                    }
+                    
+                    if (array[i] < array[k] && array[i] < min)
+                    {
+                        min = array[i];
+                    }
+                }
+
+                // Sum
+                sum += array[i];
+
+                // Odd
+                if (array[i] % 2 != 0)
+                {
+                    oddNumbers = oddNumbers + array[i] + " ";
+                }
+            }
+            mean = (double)sum / array.Length;
+            Console.WriteLine($"Max element {max}\nMin element {min}\nOdd numbers {oddNumbers}\nSum {sum}\nArithmetical mean {mean}");
+
+        
         }
     }
 }
