a83abb689e6aa7b41165852f55b36604ca105fa5
Tanya <tanyaryzhik33@gmail.com>
2019-04-18 18:57:35 +0300

    Task2 (count symbol in text)

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,32 +10,33 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input radius");
-            string radiusString = Console.ReadLine();
-            int.TryParse(radiusString, out int radius);
-            Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
-            Console.ReadLine();
+            //Console.WriteLine("Input radius");
+            //string radiusString = Console.ReadLine();
+            //int.TryParse(radiusString, out int radius);
+            //Console.WriteLine($"The square of the round is {Program.PerformCalculation(radius)}");
+            //Console.ReadLine();
 
-            //task1
-            int uberflu;
-            int _Identifier;
-            int \u006fIdentifier;
-            int myVar;
-            int myVariab1le;
+            ////task1
+            //int uberflu;
+            //int _Identifier;
+            //int \u006fIdentifier;
+            //int myVar;
+            //int myVariab1le;
 
-            //task2
-            double pi = 3.141592653;
-            double e = 2.7182818284590452;
-            Console.WriteLine($"Pi: {pi}");
-            Console.WriteLine($"e: {e}");
+            ////task2
+            //double pi = 3.141592653;
+            //double e = 2.7182818284590452;
+            //Console.WriteLine($"Pi: {pi}");
+            //Console.WriteLine($"e: {e}");
 
-            //task3
-            string str1 = "\nмоя строка";
-            string str2 = "\tмоя строка";
-            string str3 = "\aмоя строка";
-            Console.WriteLine(str1);
-            Console.WriteLine(str2);
-            Console.WriteLine(str3);
+            ////task3
+            //string str1 = "\nмоя строка";
+            //string str2 = "\tмоя строка";
+            //string str3 = "\aмоя строка";
+            //Console.WriteLine(str1);
+            //Console.WriteLine(str2);
+            //Console.WriteLine(str3);
+            HowManySymbolInText();
             Console.ReadLine();
         }
 
@@ -44,6 +45,21 @@ namespace task1
             const decimal Pi = 3.14m;
             decimal square = Pi * r * r;
             return square;
+        }
+
+        public static void HowManySymbolInText()
+        {
+            Console.WriteLine("Enter text");
+            string text = Console.ReadLine();
+            Console.WriteLine("What symbol do you want to count?");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == symbol)
+                    count++;
+            }
+            Console.WriteLine($"Symbol {symbol} is met {count} times");
         }
     }
 }
