c4ddf7ceae6a627c6dd4affe47d5739e4b3877df
Tanya <tanyaryzhik33@gmail.com>
2019-05-04 12:33:00 +0300

    blabla

---
+++ task1/ColorSet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    static class ColorSet
+    {
+        public Color color
+        public static void Print(string text, string color)
+        {
+            Console.ForegroundColor = color.to;
+            Console.WriteLine(text);
+        }
+    }
+    enum Color
+    {
+        Red = ConsoleColor.Red,
+        Yellow = ConsoleColor.Yellow,
+        Green = ConsoleColor.Green,
+        Blue = ConsoleColor.Blue,
+        Magenta = ConsoleColor.Magenta
+    }
+}
