635d28fac1c3530a36b959ae61a593f8cd946001
Tanya <tanyaryzhik33@gmail.com>
2019-05-07 18:49:17 +0300

    Task1 part1

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,6 +10,17 @@ namespace task1
     {
         static void Main(string[] args)
         {
+            Worker firstWorker = new Worker();
+            Worker secondWorker = new Worker();
+            firstWorker.name = "John";
+            firstWorker.age = 25;
+            firstWorker.salary = 1000;
+            secondWorker.name = "Basil";
+            secondWorker.age = 26;
+            secondWorker.salary = 2000;
+            Console.WriteLine(firstWorker.age + secondWorker.age);
+            Console.WriteLine(firstWorker.salary + secondWorker.salary);
+
 
         }
     }
---
+++ task1/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Worker
+    {
+        public string name;
+        public int age;
+        public decimal salary;
+    }
+}
