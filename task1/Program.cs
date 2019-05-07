0f47da799c92808bcb6c3d9cef2d83589f27b725
Tanya <tanyaryzhik33@gmail.com>
2019-05-07 19:06:50 +0300

    Add validation of age

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,18 +10,8 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Worker firstWorker = new Worker();
-            Worker secondWorker = new Worker();
-            firstWorker.name = "John";
-            firstWorker.age = 25;
-            firstWorker.salary = 1000;
-            secondWorker.name = "Basil";
-            secondWorker.age = 26;
-            secondWorker.salary = 2000;
-            Console.WriteLine(firstWorker.age + secondWorker.age);
-            Console.WriteLine(firstWorker.salary + secondWorker.salary);
-
-
+            Worker firstWorker = new Worker("John", 25, 1000);
+            Worker secondWorker = new Worker("Vasya", 26, 2000);
         }
     }
 }
--- task1/Worker.cs
+++ task1/Worker.cs
@@ -8,8 +8,27 @@ namespace task1
 {
     class Worker
     {
-        public string name;
-        public int age;
-        public decimal salary;
+        public string Name { get; set; }
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 1 && value < 100)
+                    age = value;
+            }
+        }
+        public decimal Salary { get; set; }
+
+        public Worker (string name, int age, decimal salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
     }
 }
