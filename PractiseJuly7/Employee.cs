d494259f91ac006b9bc96e6436dbea09473c4448
Tanya <tanyaryzhik33@gmail.com>
2019-07-10 20:42:14 +0300

    SlideTask

---
+++ PractiseJuly7/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseJuly7
+{
+    public class Employee
+    {
+        public Int32 Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+        public static IEnumerable<Employee> GetEmployees()
+        {
+            return new List<Employee>
+            {
+               new Employee{Id = 1, FirstName = "Eddard", LastName = "Stark", Gender = "M", BirthDate = new DateTime(1950,1,25)},
+               new Employee{Id = 2, FirstName = "Jon", LastName = "Snow", Gender = "M", BirthDate = new DateTime(1975,5,10)},
+               new Employee{Id = 3, FirstName = "Tyron", LastName = "Lannister", Gender = "M", BirthDate = new DateTime(1967,8,12)},
+               new Employee{Id = 4, FirstName = "Cersei", LastName = "Lannister", Gender = "F", BirthDate = new DateTime(1960,12,6)}
+            };
+        }
+    }
+}
---
+++ PractiseJuly7/EmployeeFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseJuly7
+{
+    public class EmployeeFilter
+    {
+        public delegate bool EmpFilter(Employee employee);
+        public static IEnumerable<Employee> Filter(IEnumerable<Employee> employees, EmpFilter filter)
+        {
+            var filteredEmployees = new List<Employee>();
+            foreach (var item in employees)
+            {
+                if (filter(item))
+                    filteredEmployees.Add(item);
+            }
+            return filteredEmployees;
+        }
+    }
+}
