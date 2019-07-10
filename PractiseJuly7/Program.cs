59521fedb51a9a8abba50586a5afc2ffb2e10060
Tanya <tanyaryzhik33@gmail.com>
2019-07-10 23:13:13 +0300

    fixing

--- PractiseJuly7/Program.cs
+++ PractiseJuly7/Program.cs
@@ -19,24 +19,30 @@ namespace PractiseJuly7
         public static void DeferedExample()
         {
             var employees = Employee.GetEmployees();
+
             Console.WriteLine("In alphabetical order:");
             var alphabeticalOrderEmployees = employees.OrderBy(e => e.LastName);
             Display(alphabeticalOrderEmployees);
+
             Console.WriteLine("Older then 30:");
             var olderEmployees = employees.Where(e => e.Age > 30);
             Display(olderEmployees);
+
             Console.WriteLine("Men:");
             var maleEmployees = employees.Where(e => e.Gender == "M");
             Display(maleEmployees);
+
             Console.WriteLine("Women:");
             var femaleEmployees = employees.Where(e => e.Gender == "F");
             Display(femaleEmployees);
+
             Console.WriteLine("Employees:");
             var employeesNames = employees.Select(e => e.FirstName + " " + e.LastName);
             foreach (var item in employeesNames)
             {
                 Console.WriteLine(item);
             }
+
             Console.WriteLine("Woman with name from 'O' and older 20:");
             var filteredEmployees = employees.Where(e => e.Gender == "F" && e.FirstName.StartsWith("O") && e.Age > 20);
             foreach (var item in filteredEmployees)
@@ -57,6 +63,7 @@ namespace PractiseJuly7
                 }
                 Console.WriteLine($"{g.Count()} employees");
             }
+
             Console.WriteLine("Second way");
             var companyGroupsSecond = employees.GroupBy(e => e.Company).Select(g => new { Name = g.Key, Count = g.Count() });
             foreach (var item in companyGroupsSecond)
