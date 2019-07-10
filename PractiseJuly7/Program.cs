51144c5b65306908d907a14e6c0e59137795057e
Tanya <tanyaryzhik33@gmail.com>
2019-07-10 20:56:05 +0300

    ClassWork

--- PractiseJuly7/Program.cs
+++ PractiseJuly7/Program.cs
@@ -8,15 +8,29 @@ namespace PractiseJuly7
 {
     class Program
     {
-               static void Main(string[] args)
+        static void Main(string[] args)
         {
-            
-            
-            foreach (var item in EmployeeFilter.Filter(Employee.GetEmployees(), filter => filter.LastName == "Lannister"))
+            //foreach (var item in EmployeeFilter.Filter(Employee.GetEmployees(), filter => filter.LastName == "Lannister"))
+            //{
+            //    Console.WriteLine($"{item.FirstName}, {item.BirthDate.ToShortDateString()}");
+            //}
+            DeferedExample();
+        }
+        public static void DeferedExample()
+        {
+            var employees = Employee.GetEmployees().ToList();
+            var maleEmployees = employees.Where(e => e.Gender == "M").ToList();
+            Display(maleEmployees);
+            employees.Add(new Employee { FirstName = "Rob", LastName = "Starck", Gender = "M" });
+            Display(maleEmployees);
+        }
+
+        private static void Display(IEnumerable<Employee> maleEmployees)
+        {
+            foreach (var item in maleEmployees)
             {
-                Console.WriteLine($"{item .FirstName}, {item.BirthDate.ToShortDateString()}");
+                Console.WriteLine($"{item.FirstName}, {item.LastName}");
             }
-            
         }
     }
 }
