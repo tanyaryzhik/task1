6e6f24f4e3c6f78e42c8fe32aea64ac4081c94f6
Tanya <tanyaryzhik33@gmail.com>
2019-07-15 23:36:31 +0300

    Idisposable started

---
+++ MyProject/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public class MyClass : IDisposable
+    {
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+
+            }
+
+            disposed = true;
+        }
+
+        ~MyClass()
+        {
+            Dispose(false);
+        }
+    }
+}
