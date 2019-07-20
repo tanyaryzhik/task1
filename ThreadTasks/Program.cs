522129703ec3222d1c7b73ba47a3392645f7d682
Tanya <tanyaryzhik33@gmail.com>
2019-07-20 13:56:43 +0300

    ThreadTask1

---
+++ ThreadTasks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(Count));
+            thread.Start();
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("First thread: " + i * i);
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Count()
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Second thread: " + i * i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
