9eb3dfced09ea98b39bed7ea60547ecd84c115ff
Tanya <tanyaryzhik33@gmail.com>
2019-07-20 14:11:18 +0300

    ThreadTask2

--- ThreadTasks/Program.cs
+++ ThreadTasks/Program.cs
@@ -11,20 +11,22 @@ namespace ThreadTasks
     {
         static void Main(string[] args)
         {
-            Thread thread = new Thread(new ThreadStart(Count));
-            thread.Start();
+            int n = 15;
+            Thread thread = new Thread(new ParameterizedThreadStart(Count));
+            thread.Start(n);
             for (int i = 1; i < 5; i++)
             {
-                Console.WriteLine("First thread: " + i * i);
+                Console.WriteLine("First thread: " + i * n);
                 Thread.Sleep(100);
             }
         }
 
-        static void Count()
+        static void Count(object x)
         {
+            int n = (int)x;
             for (int i = 1; i < 5; i++)
             {
-                Console.WriteLine("Second thread: " + i * i);
+                Console.WriteLine("Second thread: " + i * n);
                 Thread.Sleep(100);
             }
         }
